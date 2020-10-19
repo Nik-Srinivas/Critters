73226929ab6644a9eb9011ea0daee9feb00e3494
Reza <rezamohideen@utexas.edu>
2020-10-19 17:30:34 -0500

    new critters

---
+++ src/assignment4/Critter3.java
@@ -0,0 +1,23 @@
+package assignment4;
+
+public class Critter3 extends Critter{
+    // Critter1 reproduces unless direction = 2, then it runs
+    @Override
+    public void doTimeStep() {
+        int dir = getRandomInt(8);
+        if (dir == 2) {run(dir);}
+        else {reproduce(this,dir);};
+    }
+
+    // only fights Critter4
+    @Override
+    public boolean fight(String oponent) {
+        if (oponent.equals("4")) return true;
+
+        return false;
+    }
+
+    public String toString() {
+        return "1";
+    }
+}
